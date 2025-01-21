ae91f385edd93b79c1b1d2bec0feb04524440bc3
KingRocco21 <anthony21@pagone.com>
2025-01-21 09:14:12 -0800

    Replaced nonfunctional code with example code

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,35 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, languages, commands, Disposable, workspace, window } from 'vscode';
+import { CodelensProvider } from './CodelensProvider';
 
-class CodeLensProvider implements vscode.CodeLensProvider {
-	public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken):
-		vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
-			let codeLenses: vscode.CodeLens[] = [];
-			return codeLenses;
-		}
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 
-public resolveCodeLens?(codeLens: vscode.CodeLens, token: vscode.CancellationToken):
-	vscode.CodeLens | Thenable<vscode.CodeLens> {
-		return codeLens;
+let disposables: Disposable[] = [];
+
+export function activate(_context: ExtensionContext) {
+	const codelensProvider = new CodelensProvider();
+
+	languages.registerCodeLensProvider("*", codelensProvider);
+
+	commands.registerCommand("codelens-sample.enableCodeLens", () => {
+		workspace.getConfiguration("codelens-sample").update("enableCodeLens", true, true);
+	});
+
+	commands.registerCommand("codelens-sample.disableCodeLens", () => {
+		workspace.getConfiguration("codelens-sample").update("enableCodeLens", false, true);
+	});
+
+	commands.registerCommand("codelens-sample.codelensAction", (args: unknown) => {
+		window.showInformationMessage(`CodeLens action clicked with args=${args}`);
+	});
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+	if (disposables) {
+		disposables.forEach(item => item.dispose());
 	}
+	disposables = [];
 }
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext): void {
-	context.subscriptions.push(
-		vscode.languages.registerCodeLensProvider(
-			{ scheme: 'file', language: 'java' }, new CodeLensProvider()
-		)
-	);
-}
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
