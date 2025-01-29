e89273124a6f7688891f5a3478bab4564b855761
KingRocco21 <anthony21@pagone.com>
2025-01-29 10:01:37 -0800

    Added test webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,45 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, languages, commands, Disposable, workspace, window } from 'vscode';
+import * as vscode from 'vscode';
 import { CodelensProvider } from './CodelensProvider';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-let disposables: Disposable[] = [];
-
-export function activate(_context: ExtensionContext) {
+export function activate(_context: vscode.ExtensionContext) {
 	const codelensProvider = new CodelensProvider();
 
-	// Change to 'java'
-	languages.registerCodeLensProvider('java', codelensProvider);
+	_context.subscriptions.push(
+		// Change to 'java'
+		vscode.languages.registerCodeLensProvider('java', codelensProvider));
 
-	commands.registerCommand("wpilibj-integrated-docs.enableCodeLens", () => {
-		workspace.getConfiguration("wpilibj-integrated-docs").update("enableCodeLens", true, true);
-	});
+	_context.subscriptions.push(
+		vscode.commands.registerCommand("wpilibj-integrated-docs.codelensAction", (args: unknown) => {
+			vscode.commands.executeCommand("wpilibj-integrated-docs.openDocs");
+			//commands.executeCommand('vscode.open', "https://docs.wpilib.org/en/stable/docs/software/labview/index.html");
+		})
+	);
 
-	commands.registerCommand("wpilibj-integrated-docs.disableCodeLens", () => {
-		workspace.getConfiguration("wpilibj-integrated-docs").update("enableCodeLens", false, true);
-	});
+	_context.subscriptions.push(
+		vscode.commands.registerCommand("wpilibj-integrated-docs.openDocs", (args: unknown) => {
+			const panel: vscode.WebviewPanel = vscode.window.createWebviewPanel('docsView', 'Docs View', vscode.ViewColumn.Two, {});
+			panel.webview.html = getWebviewContent();
+			
+		})
+	);
+}
 
-	commands.registerCommand("wpilibj-integrated-docs.codelensAction", (args: unknown) => {
-		commands.executeCommand('vscode.open', "https://docs.wpilib.org/en/stable/docs/software/labview/index.html");
-	});
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Test</title>
+</head>
+<body style="background-color:#e0e0e0">
+	<iframe src="https://docs.wpilib.org/en/stable/docs/software/labview/index.html" style="border:none;position:absolute;height:100%;" title="Labview" width="100%"></iframe>
+</body>
+</html>`;
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-	if (disposables) {
-		disposables.forEach(item => item.dispose());
-	}
-	disposables = [];
-}
+export function deactivate() {}
